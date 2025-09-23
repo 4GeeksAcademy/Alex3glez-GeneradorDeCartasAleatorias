ce13137a8555dfeae663abbba10288946fd36af1
Alex3glez <a5l6e7x8@gmail.com>
2025-09-23 04:31:50 +0000

    casi terminado

--- src/app.js
+++ src/app.js
@@ -2,10 +2,75 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const picas = "assets/img/picas-removebg-preview.png";
+const corazones = "assets/img/corazones-removebg-preview.png";
+const rombos = "assets/img/rombos-removebg-preview.png";
+const trebol = "assets/img/trebol-removebg-preview.png";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const numeroCarta = document.getElementsByClassName("numero");
+const paloArriba = document.querySelector(".paloArriba img");
+const paloAbajo = document.querySelector(".paloAbajo img");
+const btnAleatorio = document.getElementById("btnAleatorio");
+const btnTemporizador = document.getElementById("btnTemporizador");
+const temporizadorSegundos = document.getElementById("temporizador");
+const inputAlto=document.getElementById("estableceAlto");
+const inputAncho=document.getElementById("estableceAncho");
+const carta=document.querySelector(".tarjeta");
+let intervalo;
+
+
+
+function paloAleatorio() {
+    let palosArr = [picas, corazones, rombos, trebol];
+    let num = Math.floor(Math.random() * 4)
+    paloArriba.src = palosArr[num];
+    paloAbajo.src = palosArr[num];
+}
+function valorCartaAleatorio() {
+    let valorCarta = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+    let num = Math.floor(Math.random() * valorCarta.length);
+    numeroCarta[0].textContent = valorCarta[num]
+
+
+}
+
+function todoAleatorio() {
+    valorCartaAleatorio();
+    paloAleatorio();
+
+}
+
+function iniciarTemporizador() {
+    let seconds = 10;
+
+    clearInterval(intervalo);
+
+    intervalo = setInterval(() => {
+        if (seconds === 0) {
+            todoAleatorio();
+            seconds = 10;
+            clearInterval(intervalo)
+        }
+        else { seconds-- }
+        temporizadorSegundos.textContent = seconds;
+
+    }, 1000);
+
+}
+
+function cambiarAncho(e) {
+   if (e.key=="Enter") carta.style.width= inputAlto.value+"px";
+    
+}
+function cambiarAlto(e) {
+   if (e.key=="Enter") carta.style.height= inputAlto.value+"px";
+    
+}
+
+btnAleatorio.addEventListener('click', todoAleatorio);
+btnTemporizador.addEventListener('click', iniciarTemporizador);
+inputAncho.addEventListener('keydown', cambiarAncho)
+inputAlto.addEventListener('keydown', cambiarAlto)
+
+
+
